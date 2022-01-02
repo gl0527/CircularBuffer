0a52378242467f0aa6e58c85354f2a2444ec5f0f
gl0527 <gurzolajos27@gmail.com>
2022-01-02 16:49:27 +0100

    Add documentation

--- include/circular_buffer/circular_buffer.hpp
+++ include/circular_buffer/circular_buffer.hpp
@@ -1,24 +1,75 @@
+/**
+ * @file circular_buffer.hpp
+ * @brief Simple circular buffer implementation.
+ *
+ * @author gl0527 <gurzolajos27@gmail.com>
+ * @date 02 Jan 2022
+ */
+
 #pragma once
 
 #include <cassert>
 
+/**
+ * @brief Circular buffer implementation.
+ * @tparam T Type of buffered elements.
+ * @tparam N Size of the buffer.
+ */
 template<typename T, std::size_t N>
 class CircularBuffer {
 public:
+    /**
+     * @brief Adds an element to the buffer (after the last element).
+     * @param[in] elem The element to be added to the buffer.
+     * @param[in] overwrite A flag which determines what should happen if the buffer is full.
+     *                      If true, subsequent elements will overwrite previously added ones.
+     *                      If false, subsequent elements get ignored.
+     * @return Void.
+     */
     void push(T const& elem, bool overwrite = true) noexcept;
+
+    /**
+     * @brief Gives back the first element of the buffer, and also removes it from there.
+     * @return The first element of the buffer.
+     */
     T pop() noexcept;
+
+    /**
+     * @brief Resets the buffer to its initial state.
+     * This function only resets the indices and size variables, but does not alter the buffer.
+     * @return Void.
+     */
     void reset() noexcept;
 
+    /**
+     * @brief Gives back the number of elements in the buffer.
+     * @return The number of elements in the buffer.
+     */
     std::size_t size() const noexcept;
+
+    /**
+     * @brief Gives back the maximum number of elements in the buffer.
+     * @return The maximum number of elements in the buffer.
+     */
     constexpr std::size_t capacity() const noexcept;
+
+    /**
+     * @brief Checks if the buffer is empty.
+     * @return True if the buffer is empty, false otherwise.
+     */
     bool empty() const noexcept;
+
+    /**
+     * @brief Checks if the buffer is full.
+     * @return True if the buffer is full, false otherwise.
+     */
     bool full() const noexcept;
 
 private:
-    T m_buffer[N]{};
-    std::size_t m_data_start{0};
-    std::size_t m_data_end{0};
-    std::size_t m_size{0};
+    T m_buffer[N]{};                ///< The array which contains all the elements.
+    std::size_t m_data_start{0};    ///< The index where the data starts.
+    std::size_t m_data_end{0};      ///< The index after the last element of the data.
+    std::size_t m_size{0};          ///< The number of elements in the array.
 };
 
 template<typename T, std::size_t N>
